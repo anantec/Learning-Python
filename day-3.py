5b52f1a626f0e212d2cbf2e270f200aeecb6ea62
Anant <asingh1037125@gmail.com>
2024-07-01 13:35:39 +0530

    this is day 3 of learning python from scratch

--- day-3.py
+++ day-3.py
@@ -1,1 +1,31 @@
-# this is just a testing file to check whether i can push another file or not
+#  we are going to learn about some topic like boolean and or not
+
+a1 = 7>2
+
+print(a1)
+
+#  now we will see some program about AND OR NOT 
+
+a2 = (7>5) and (5>7)
+
+print(a2)
+# we'll get our output as false cause in AND conditons both the conditions need to be true if any one of them is false then output will be false
+
+#  now we'll look for OR operator 
+ 
+a3 = (10>9) or (20>100)
+
+#  according to me the output will be true because there one condition need to be true 
+
+print(a3)
+# the output is true in OR case if both the condtion is false then the output will be false otherwise the output will be true
+
+# now we'll look at NOT operator 
+
+a4= (7>3)
+# the above condition is true 
+
+a5 =not a4
+
+print(a5)
+# the above condition will change the output to its opposite for example true will be false and false will be true
