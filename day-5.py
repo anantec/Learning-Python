3eae8e8151c6da2f6278e429be98c712ba10d36e
Anant <asingh1037125@gmail.com>
2024-07-03 19:04:35 +0530

    this is day 5 of learning python

---
+++ day-5.py
@@ -0,0 +1,27 @@
+# day 5 of learning python
+
+# today we learned about list in pyhton in other laguage we may call it as array
+
+# we intialize a list by puting items inside []
+
+sample = ["this is ", "sample","of list"]
+
+# we can simply call it to print all the items
+print(sample);
+
+# there are some methods for list in python
+# some methods like: pop,insert,append
+sample.append("Created")
+print(sample)
+
+sample.insert(3,"in python")
+print(sample)
+
+sample.pop()
+print(sample)
+
+sm1="By Anant"
+
+sm2= sample.append(sm1)
+
+print(sample)
