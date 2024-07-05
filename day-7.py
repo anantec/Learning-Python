5f867f9fa777892ab6e5f98f8744236af48a6f7c
Anant <asingh1037125@gmail.com>
2024-07-05 09:57:30 +0530

    day 7 of learning python from scratch

---
+++ day-7.py
@@ -0,0 +1,32 @@
+# this day 7 of learning python from scratch
+
+# today I saw some basic concept of looping
+
+# first we are going to give a look on for loop
+
+list=["1","2","3","4","5"]
+
+for i in list :
+    print(i)
+    i=+1
+
+# the above code will print all the items in list until the list ends
+
+
+print("\n")
+print("for loop with an increment of 1")
+
+
+
+for i in range(10,21):
+    print(i)
+
+# it will automatically increment the variable or value by 1 and the above code says that the  value will start from  10 and ends at 20
+
+
+# now i am going ro write a code for increment of 2
+print("\n")
+print("for loop with an increment of 2")
+
+for i in range(0,20,2):
+    print(i)
